21da84ca873a704bbaf04eb4032dd90775157593
lydia385 <78177457+lydia385@users.noreply.github.com>
2023-01-28 22:23:36 +0100

    final

--- Comunication-multimedia/bloc-matching/main.py
+++ Comunication-multimedia/bloc-matching/main.py
@@ -25,8 +25,11 @@ def meanSquaredError(img1, img2):
         return err
 
 # def meanSquaredError(img1,img2):
-#         mse = mean_squared_error(img1, img2)
-#         return mse
+#    h, w ,_ = img1.shape
+#    diff = cv2.subtract(img1, img2)
+#    err = np.sum(diff**2)
+#    mse = err/(float(h*w))
+#    return mse
 
 
 
@@ -49,31 +52,40 @@ frame_carre = np.copy(img2)
 
 
 # Divide the frame(395, 703, 3) into blocks 16*16
-for i in range(0, frame.shape[0]-16, block_size[0]):
+for i in range(0,frame.shape[0]-16, block_size[0]):
     for j in range(0, frame.shape[1]-16, block_size[1]):
+# for i in range(150,151, block_size[0]):
+#     for j in range(120, 121, block_size[1]):
         i_base=i
         j_base=j
+        print("luda",i_base,j_base)
         # Get the current block
-        targetBlock  = frame[i:i+block_size[0], j:j+block_size[1]]
+        targetBlock  = prev_frame[i:i+block_size[0], j:j+block_size[1]]
 
         # Search for the most similar block in the previous frame
         min_mse = float("inf")
         min_x = 0
         min_y = 0
         search_size = 32
-        while search_size>=1 and i_base<=frame.shape[0]+block_size[0] and j_base<= frame.shape[1]+block_size[1]:
-            for x in range(i-search_size,i+search_size*2,search_size):
-                for y in range(j-search_size,j+search_size*2,search_size):
-                        if(x>=0 and y>=0 and (x!=i or y!=j) and x+block_size[0]<frame.shape[0] and y+block_size[1]<frame.shape[1]):
+        while search_size>=16 and i_base<=frame.shape[0]+block_size[0] and j_base<= frame.shape[1]+block_size[1]:
+            for x in range(i_base-search_size,i_base+search_size+1,search_size):
+                for y in range(j_base-search_size,j_base+search_size+1,search_size):
+                        if(x>=0 and y>=0 and x+block_size[0]<frame.shape[0] and y+block_size[1]<frame.shape[1]):
                             SearchBlock=frame[x:x+block_size[0],y:y+block_size[1]]
+                            
+                            #cv2.rectangle(frame_carre, (y, x), (y+block_size[1], x+block_size[0]), (255, 255, 255), 2)
+                            #cv2.rectangle(frame_carre, (y, x), (y+16, x+16), (200, 0, 0), 2)
                             mse = meanSquaredError(targetBlock , SearchBlock)
                             if mse < min_mse:
                                 min_mse = mse
                                 min_x = x
                                 min_y = y
-           
+                            #print(x,y,i_base,j_base,mse)
+            
+
             i_base=min_x
             j_base=min_y
+            #print("/n",i_base,j_base,i,j,block_size)
             search_size=search_size//2
             
         
@@ -81,13 +93,14 @@ for i in range(0, frame.shape[0]-16, block_size[0]):
         #print(i,j,min_x,min_y)
         if (min_mse>50):
          # Calculate the residual
-         black_img[i:i+block_size[0],j:j+block_size[1]] = np.subtract(img2[min_x:min_x+block_size[0],min_y:min_y+block_size[1]],img[i:i+block_size[0], j:j+block_size[1]])
+         #black_img[i:i+block_size[0],j:j+block_size[1]] = np.subtract(img2[min_x:min_x+block_size[0],min_y:min_y+block_size[1]],img[i:i+block_size[0], j:j+block_size[1]])
+         black_img[i:i+block_size[0],j:j+block_size[1]] = img[i:i+block_size[0], j:j+block_size[1]]
          #  img2[min_x:min_x+block_size[0],min_y:min_y+block_size[1]] + img[i:i+block_size[0], j:j+block_size[1]]
         else :
          # Surround the block with a square 
          cv2.rectangle(frame_carre, (min_y, min_x), (min_y+block_size[1], min_x+block_size[0]), (0, 255, 0), 2)
         
-        cv2.rectangle(prev_frame_carre, (j, i), (j+block_size[1], i+block_size[0]), (0, 0, 0), 2)
+        cv2.rectangle(prev_frame_carre, (j, i), (j+block_size[1], i+block_size[0]), (255, 255, 255), 2)
 
 
     
